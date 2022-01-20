e88db8ca69acaf629fbb220d8017ac8cd56c603e
Gabriela Melnik Carvalho <gabrielamcarvalho@gmail.com>
2022-01-20 14:41:39 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae tempora
+        perspiciatis qui aspernatur adipisci recusandae reiciendis rerum?
+        Temporibus necessitatibus dignissimos possimus excepturi quo voluptates
+        harum delectus, vero adipisci aliquid vitae.
+      </p>
+    </section>
+  );
+};
+
+export default About;
